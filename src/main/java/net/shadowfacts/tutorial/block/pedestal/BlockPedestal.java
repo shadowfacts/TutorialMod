53d457fa1902683d9628f793b64ce4569d939a83
Shadowfacts <me@shadowfacts.net>
2016-12-04 11:17:50 -0500

    Tile Entities w/ inventory GUI

--- src/main/java/net/shadowfacts/tutorial/block/pedestal/BlockPedestal.java
+++ src/main/java/net/shadowfacts/tutorial/block/pedestal/BlockPedestal.java
@@ -14,6 +14,7 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.shadowfacts.tutorial.ModGuiHandler;
 import net.shadowfacts.tutorial.TutorialMod;
 import net.shadowfacts.tutorial.block.BlockBase;
 import net.shadowfacts.tutorial.block.BlockTileEntity;
@@ -50,13 +51,7 @@ public class BlockPedestal extends BlockTileEntity<TileEntityPedestal> {
 					player.setHeldItem(hand, itemHandler.insertItem(0, heldItem, false));
 				}
 			} else {
-				ItemStack stack = itemHandler.getStackInSlot(0);
-				if (stack != null) {
-					String localized = TutorialMod.proxy.localize(stack.getUnlocalizedName() + ".name");
-					player.addChatMessage(new TextComponentString(stack.stackSize + "x " + localized));
-				} else {
-					player.addChatMessage(new TextComponentString("Empty"));
-				}
+				player.openGui(TutorialMod.instance, ModGuiHandler.PEDESTAL, world, pos.getX(), pos.getY(), pos.getZ());
 			}
 		}
 		return true;
---
+++ src/main/java/net/shadowfacts/tutorial/block/pedestal/ContainerPedestal.java
@@ -0,0 +1,75 @@
+package net.shadowfacts.tutorial.block.pedestal;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.*;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+
+public class ContainerPedestal extends Container {
+
+	public ContainerPedestal(InventoryPlayer playerInv, final TileEntityPedestal pedestal) {
+		IItemHandler inventory = pedestal.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+		addSlotToContainer(new SlotItemHandler(inventory, 0, 80, 35) {
+			@Override
+			public void onSlotChanged() {
+				pedestal.markDirty();
+			}
+		});
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 9; j++) {
+				addSlotToContainer(new Slot(playerInv, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+			}
+		}
+
+		for (int k = 0; k < 9; k++) {
+			addSlotToContainer(new Slot(playerInv, k, 8 + k * 18, 142));
+		}
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer player) {
+		return true;
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+		ItemStack itemstack = null;
+		Slot slot = inventorySlots.get(index);
+
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			int containerSlots = inventorySlots.size() - player.inventory.mainInventory.length;
+
+			if (index < containerSlots) {
+				if (!this.mergeItemStack(itemstack1, containerSlots, inventorySlots.size(), true)) {
+					return null;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 0, containerSlots, false)) {
+				return null;
+			}
+
+			if (itemstack1.stackSize == 0) {
+				slot.putStack(null);
+			} else {
+				slot.onSlotChanged();
+			}
+
+			if (itemstack1.stackSize == itemstack.stackSize) {
+				return null;
+			}
+
+			slot.onPickupFromSlot(player, itemstack1);
+		}
+
+		return itemstack;
+	}
+
+
+}
